aca618e93db4ab5f8ba0eea8a22e1c9cffe8c9c5
Vlad Kostarev <vladsonic23@yandex.ru>
2017-09-18 14:26:49 +0300

    I did everything. Can I do the next lessons?

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -44,7 +44,10 @@ fun ageDescription(age: Int): String = TODO()
  */
 fun timeForHalfWay(t1: Double, v1: Double,
                    t2: Double, v2: Double,
-                   t3: Double, v3: Double): Double = TODO()
+                   t3: Double, v3: Double): Double
+        {
+
+        }
 
 /**
  * Простая
